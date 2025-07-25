b6f398768ab7fcee1fd3b679cdd49c4b19670334
nicovidal <nico.vidal.m@gmail.com>
2025-07-25 12:29:41 -0400

    Nbar

--- src/QrApp.jsx
+++ src/QrApp.jsx
@@ -6,14 +6,32 @@ export const QrApp = () => {
   return (
     <Router>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-3">
-        <Link className="navbar-brand" to="/">QR App</Link>
-        <div className="collapse navbar-collapse">
+        <Link className="navbar-brand" to="/">
+          QR App
+        </Link>
+
+        {/* Botón hamburguesa */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"ks
+          data-bs-target="#navbarNav"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        {/* Contenido colapsable */}
+        <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav me-auto">
             <li className="nav-item">
-              <Link className="nav-link" to="/">html5-qrcode</Link>
+              <Link className="nav-link" to="/">
+                html5-qrcode
+              </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/react-zxing">react-zxing</Link>
+              <Link className="nav-link" to="/react-zxing">
+                react-zxing
+              </Link>
             </li>
           </ul>
         </div>
